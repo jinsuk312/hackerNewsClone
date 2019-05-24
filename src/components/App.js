5c648d90cf47be19f58b569e57e9b265c5cd3f5d
jinsukmik <jinsukmik@gmail.com>
2019-05-24 17:42:18 -0500

    added folder structure, main compnents and component routes

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import CreateLink from "./Link/CreateLink";
+import Login from "./Auth/Login";
+import ForgotPassword from "./Auth/ForgotPassword";
+import SearchLink from "./Link/SearchLinks";
+import LinkList from "./Link/LinkList";
+import LinkDetail from "./Link/LinkDetail";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+        <Route path="/create" component={CreateLink} />
+        <Route path="/login" component={Login} />
+        <Route path="/forgot" component={ForgotPassword} />
+        <Route path="/search" component={SearchLink} />
+        <Route path="/top" component={LinkList} />
+        <Route path="/new/:page" component={LinkList} />
+        <Route path="/link/:linkId" component={LinkDetail} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
