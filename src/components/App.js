1a5d5c08fb9bf19bec8a2fbd3c35cb4fd8759156
jinsukmik <jinsukmik@gmail.com>
2019-05-25 11:46:43 -0500

    added navbar

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,25 @@ import ForgotPassword from "./Auth/ForgotPassword";
 import SearchLink from "./Link/SearchLinks";
 import LinkList from "./Link/LinkList";
 import LinkDetail from "./Link/LinkDetail";
-
+import Header from "./Header";
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/new/1" />} />
-        <Route path="/create" component={CreateLink} />
-        <Route path="/login" component={Login} />
-        <Route path="/forgot" component={ForgotPassword} />
-        <Route path="/search" component={SearchLink} />
-        <Route path="/top" component={LinkList} />
-        <Route path="/new/:page" component={LinkList} />
-        <Route path="/link/:linkId" component={LinkDetail} />
-      </Switch>
+      <div className="app-container">
+        <Header />
+        <div className="route-container">
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+            <Route path="/create" component={CreateLink} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot" component={ForgotPassword} />
+            <Route path="/search" component={SearchLink} />
+            <Route path="/top" component={LinkList} />
+            <Route path="/new/:page" component={LinkList} />
+            <Route path="/link/:linkId" component={LinkDetail} />
+          </Switch>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,36 @@
 import React from "react";
-
+import { withRouter, NavLink } from "react-router-dom";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="flex">
+        <img src="/logo.png" />
+        <NavLink to="/" className="header-title">
+          Hook News
+        </NavLink>
+        <NavLink to="/" className="header-link">
+          new
+        </NavLink>
+        <div className="divider"> | </div>
+        <NavLink to="/top" className="header-link">
+          top
+        </NavLink>
+        <div className="divider"> | </div>
+        <NavLink to="/search" className="header-link">
+          search
+        </NavLink>
+        <div className="divider"> | </div>
+        <NavLink to="/create" className="header-link">
+          create
+        </NavLink>
+      </div>
+      <div className="flex">
+        <NavLink to="/login" className="header-link">
+          login
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Header;
+export default withRouter(Header);
