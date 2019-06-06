af3cbcb384c5242da0c7fde01607a4832fb971e0
jinsukmik <jinsukmik@gmail.com>
2019-06-06 18:44:12 -0500

    firestore and creating links

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,19 +1,37 @@
 import React from "react";
 import useFormValidation from "../Auth/useFormValidation";
 import validateCreateLink from "../Auth/validateCreateLink";
-
+import FirebaseContext from "../../firebase/context";
 const INITIAL_STATE = {
   description: "",
   url: ""
 };
 function CreateLink(props) {
+  const { firebase, user } = React.useContext(FirebaseContext);
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITIAL_STATE,
     validateCreateLink,
     handleCreateLink
   );
   function handleCreateLink() {
-    console.log("link created!");
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      const { url, description } = values;
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now()
+      };
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
+    }
   }
   return (
     <form onSubmit={handleSubmit} className="flex flex-column mt3">
@@ -33,7 +51,7 @@ function CreateLink(props) {
         name="url"
         placeholder="The URL for your link"
         autoComplete="off"
-        type="text"
+        type="url"
         className={errors.url && "error-input"}
       />
       {errors.url && <p className="error-text">{errors.url}</p>}
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,12 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 import firebaseConfig from "./config";
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
     this.auth = app.auth();
+    this.db = app.firestore();
   }
   async register(name, email, password) {
     const newUser = await this.auth.createUserWithEmailAndPassword(
