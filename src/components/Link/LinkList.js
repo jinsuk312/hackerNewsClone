18709b3188c7f1df8a03f249bee53f0fc433c38b
jinsukmik <jinsukmik@gmail.com>
2019-06-06 18:52:18 -0500

    fetch links

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,5 +1,22 @@
 import React from "react";
+import FirebaseContext from "../../firebase/context";
 function LinkList(props) {
+  const { firebase } = React.useContext(FirebaseContext);
+
+  React.useEffect(() => {
+    getLinks();
+  }, []);
+  function getLinks() {
+    firebase.db.collection("links").onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return { id: doc.id, ...doc.data() };
+    });
+    console.log({ links });
+  }
+
   return <div>LinkList</div>;
 }
 
