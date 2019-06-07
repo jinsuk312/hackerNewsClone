154ff3f0ac81624f4403730383e0a0f5eef438fb
jinsukmik <jinsukmik@gmail.com>
2019-06-06 20:39:45 -0500

    comments

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -42,7 +42,9 @@ function LinkItem({ link, index, showCount, history }) {
       </div>
       <div className="ml1">
         <div>
-          {link.description}{" "}
+          <a href={link.url} className="black no-underline">
+            {link.description}
+          </a>{" "}
           <span className="link">({getDomain(link.url)})</span>
         </div>
         <div className="f6 1h-copy gray">
